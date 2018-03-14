02adbc8494c31878ace17bd9f4c63996db4af8c8
mikehopps <mhopps@gmail.com>
2018-03-14 12:23:05 -0400

    Sequential Search, Binary search.  Both with loops and recursion!

--- src/SearchMain.java
+++ src/SearchMain.java
@@ -1,20 +1,27 @@
+import java.util.Arrays;
+
 /**
  * Created by michael_hopps on 3/7/18.
  */
 public class SearchMain {
     public static void main(String[] args) {
-        int[] testArray = {4, 1, 6, 8, 9, 18, 12, -4, 5, 9, 15, 34, 42, 101};
+        int[] testArray = {-2, -1, 0, 1, 5, 8, 12, 13, 20, 24, 33, 34, 36, 42, 45, 100};
 
         Searcher mySearcher = new Searcher(testArray);
 
-        System.out.println(mySearcher.sequentialSearch(9));
-        System.out.println(mySearcher.sequentialSearchRecursive(9, 0));
-
-        System.out.println(mySearcher.sequentialSearch(9000));
-        System.out.println(mySearcher.sequentialSearchRecursive(9000, 0));
-
-        System.out.println(mySearcher.sequentialSearch(101));
-        System.out.println(mySearcher.sequentialSearchRecursive(101, 0));
+        System.out.println(mySearcher.binarySearchRecursion(8, 0, testArray.length-1));
+        System.out.println(mySearcher.binarySearchRecursion(19, 0, testArray.length-1));
+        System.out.println(mySearcher.binarySearchRecursion(100, 0, testArray.length-1));
+        System.out.println(mySearcher.binarySearchRecursion(-2, 0, testArray.length-1));
+        System.out.println(mySearcher.binarySearchRecursion(120, 0, testArray.length-1));
+//        System.out.println(mySearcher.sequentialSearch(9));
+//        System.out.println(mySearcher.sequentialSearchRecursive(9, 0));
+//
+//        System.out.println(mySearcher.sequentialSearch(9000));
+//        System.out.println(mySearcher.sequentialSearchRecursive(9000, 0));
+//
+//        System.out.println(mySearcher.sequentialSearch(101));
+//        System.out.println(mySearcher.sequentialSearchRecursive(101, 0));
 
     }
 }
--- src/Searcher.java
+++ src/Searcher.java
@@ -15,20 +15,56 @@ public class Searcher {
         this.arr = arr;
     }
 
-    //sequential
-    public int sequentialSearch(int key){
+    //returns the index of key in arr.
+    public int sequentialSearch(int key) {
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == key)
+            if (arr[i] == key)
                 return i;
         }
         return -1;
     }
 
-    public int sequentialSearchRecursive(int key, int i){
-        if(arr[i] == key)
+    public int sequentialSearchRecursive(int key, int i) {
+        if (arr[i] == key)
             return i;
-        if(i == arr.length - 1)
+        if (i == arr.length - 1)
             return -1;
         return sequentialSearchRecursive(key, i + 1);
     }
+
+    public int binarySearch(int key) {
+        int hi = arr.length - 1; //last index
+        int lo = 0; //first index
+        int mid;
+
+        while (lo <= hi) {    //if our boundaries are in order…
+
+            mid = (hi + lo) / 2;    //middle of current range.
+            if (key == arr[mid])    //found it!  Return.
+                return mid;
+            else if (key < arr[mid])    //key is less than mid – lower hi.
+                hi = mid - 1;
+            else
+                lo = mid + 1;        //key is more than mid – raise lo.
+        }
+        return -1;    //never found it.  Return -1.
+    }
+
+
+
+    public int binarySearchRecursion(int key, int low, int high){
+        if(low > high)
+            return -1;
+        int mid = (low + high) / 2;
+        if(key == arr[mid])
+            return mid;
+
+        if(key > arr[mid])
+            return binarySearchRecursion(key, mid+1, high);
+        else
+            return binarySearchRecursion(key, low, mid-1);
+    }
 }
+
+
+
